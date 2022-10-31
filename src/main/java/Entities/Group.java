25098e3d10f237bcfd19fe0eb2efc56412f47cb9
mishaalkandapath <mishaalhere@gmail.com>
2022-10-31 19:55:28 -0400

    crated groups

--- src/main/java/Entities/Group.java
+++ src/main/java/Entities/Group.java
@@ -1,4 +1,10 @@
 package Entities;
 
 public class Group {
+    private String[] users;
+    private PurchaseList purchaseList;
+    private PlanningList planningList;
+    private PurchaseBalance purchaseBalance;
+    private static String[] groups;
+
 }
