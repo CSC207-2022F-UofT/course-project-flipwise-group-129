41e0c239a15f6c18d27aea697b6562024e72c54c
mishaalkandapath <mishaalhere@gmail.com>
2022-10-31 19:47:16 -0400

    created itemlist entity instances

--- src/main/java/Entities/ItemList.java
+++ src/main/java/Entities/ItemList.java
@@ -1,4 +1,15 @@
 package Entities;
 
 public class ItemList {
+
+    private String itemName;
+    private String buyer;
+    private Float price;
+
+    public ItemList(String itemName, String buyer, Float price){
+        this.itemName = itemName;
+        this.buyer = buyer;
+        this.price = price;
+    }
+
 }
