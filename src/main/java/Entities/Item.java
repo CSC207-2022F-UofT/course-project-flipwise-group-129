edb654ed8fd454a5fc24e8329f9fc7f87c0a3d65
mishaalkandapath <mishaalhere@gmail.com>
2022-10-31 19:53:41 -0400

    created item, itemlist, planninglist, purchaselist

--- src/main/java/Entities/Item.java
+++ src/main/java/Entities/Item.java
@@ -1,4 +1,13 @@
 package Entities;
 
 public class Item {
+    private String itemName;
+    private String buyer;
+    private Float price;
+
+    public Item(String itemName, String buyer, Float price){
+        this.itemName = itemName;
+        this.buyer = buyer;
+        this.price = price;
+    }
 }
--- src/main/java/Entities/ItemList.java
+++ src/main/java/Entities/ItemList.java
@@ -1,15 +1,6 @@
 package Entities;
 
-public class ItemList {
+abstract public class ItemList {
 
-    private String itemName;
-    private String buyer;
-    private Float price;
-
-    public ItemList(String itemName, String buyer, Float price){
-        this.itemName = itemName;
-        this.buyer = buyer;
-        this.price = price;
-    }
-
+    private String[] items;
 }
