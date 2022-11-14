759fcad1f0eeeb5fed82723188f8edf1f36fd188
sopleee <leesophie99@gmail.com>
2022-11-14 18:48:59 -0500

    Added attributes, getters, setters to the addingPlanningList controller

--- src/main/java/Controllers/AddToPlanningController.java
+++ src/main/java/Controllers/AddToPlanningController.java
@@ -1,4 +1,18 @@
 package Controllers;
-
+import UseCases.AddToPlanningList;
+import DataStructures.PlannedItemInfo;
 public class AddToPlanningController {
+    public PlannedItemInfo itemInfo;
+
+    public PlannedItemInfo getItemInfo() {
+        return itemInfo;
+    }
+
+    public void setItemInfo(PlannedItemInfo itemInfo) {
+        this.itemInfo = itemInfo;
+    }
+
+    public void performPlanningAdd() {
+//        AddToPlanningList.___(itemInfo);
+    }
 }
--- src/main/java/DataStructures/PlannedItemInfo.java
+++ src/main/java/DataStructures/PlannedItemInfo.java
@@ -1,4 +1,5 @@
 package DataStructures;
 
 public class PlannedItemInfo {
+
 }
