637995152e235fdf9a0d6254321a1653589094ab
Aviraj Newatia <projectavi.exe@gmail.com>
2022-11-16 22:18:03 -0500

    Implemented structure of presenter and output boundary

--- src/main/java/Controllers/AddPurchaseController.java
+++ src/main/java/Controllers/AddPurchaseController.java
@@ -5,12 +5,14 @@ import Entities.Group;
 import Entities.Item;
 import Entities.User;
 import InputBoundary.AddPurchaseBoundaryIn;
+import OutputBoundary.AddPurchaseBoundaryOut;
 
 import java.util.List;
 
 public class AddPurchaseController {
 
     private AddPurchaseBoundaryIn addPurchaseUseCaseBoundaryIn;
+    private AddPurchaseBoundaryOut addPurchaseBoundaryOut;
 
     public void controlAddPurchaseUseCase(Item purchasedItem, List<User> participatingUsers, User buyer, float price, Group purchaseGroup) {
         PurchaseInfo purchaseInfo = new PurchaseInfo(purchasedItem, participatingUsers, buyer, price, purchaseGroup);
--- src/main/java/Presenters/AddPurchasePresenter.java
+++ src/main/java/Presenters/AddPurchasePresenter.java
@@ -1,9 +1,15 @@
 package Presenters;
 
 import DataStructures.UpdatedLists;
+import InputBoundary.AddPurchaseBoundaryIn;
+import OutputBoundary.AddPurchaseBoundaryOut;
 
-public class AddPurchasePresenter {
-    private UpdatedLists newLists;
+public class AddPurchasePresenter implements AddPurchaseBoundaryOut {
+
+    @Override
+    public void updateView(UpdatedLists updatedLists) {
+        // Update the view
+    }
 
 //    speak to the view
 }
