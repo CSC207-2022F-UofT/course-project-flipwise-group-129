793b5367610ccc6c2fbc163487bdb7385e220fbd
Aviraj Newatia <projectavi.exe@gmail.com>
2022-11-16 22:26:22 -0500

    Initialised the presenter within the controller to pass along the pipeline

--- src/main/java/Controllers/AddPurchaseController.java
+++ src/main/java/Controllers/AddPurchaseController.java
@@ -6,15 +6,16 @@ import Entities.Item;
 import Entities.User;
 import InputBoundary.AddPurchaseBoundaryIn;
 import OutputBoundary.AddPurchaseBoundaryOut;
+import Presenters.AddPurchasePresenter;
 
 import java.util.List;
 
 public class AddPurchaseController {
 
     private AddPurchaseBoundaryIn addPurchaseUseCaseBoundaryIn;
-    private AddPurchaseBoundaryOut addPurchaseBoundaryOut;
 
     public void controlAddPurchaseUseCase(Item purchasedItem, List<User> participatingUsers, User buyer, float price, Group purchaseGroup) {
+        AddPurchaseBoundaryOut presenter = new AddPurchasePresenter();
         PurchaseInfo purchaseInfo = new PurchaseInfo(purchasedItem, participatingUsers, buyer, price, purchaseGroup, presenter);
 
         addPurchaseUseCaseBoundaryIn.executeUseCase(purchaseInfo);
