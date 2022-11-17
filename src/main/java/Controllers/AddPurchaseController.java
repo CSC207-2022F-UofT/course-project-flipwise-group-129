bb69da3f045d19a1503c1715e7460578910c1917
Aviraj Newatia <projectavi.exe@gmail.com>
2022-11-16 22:15:52 -0500

    Implemented AddPurchase controller

--- src/main/java/Controllers/AddPurchaseController.java
+++ src/main/java/Controllers/AddPurchaseController.java
@@ -1,4 +1,20 @@
 package Controllers;
 
+import DataStructures.PurchaseInfo;
+import Entities.Group;
+import Entities.Item;
+import Entities.User;
+import InputBoundary.AddPurchaseBoundaryIn;
+
+import java.util.List;
+
 public class AddPurchaseController {
+
+    private AddPurchaseBoundaryIn addPurchaseUseCaseBoundaryIn;
+
+    public void controlAddPurchaseUseCase(Item purchasedItem, List<User> participatingUsers, User buyer, float price, Group purchaseGroup) {
+        PurchaseInfo purchaseInfo = new PurchaseInfo(purchasedItem, participatingUsers, buyer, price, purchaseGroup);
+
+        addPurchaseUseCaseBoundaryIn.executeUseCase(purchaseInfo);
+    }
 }
