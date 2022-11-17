3cbe6e6b5e82aa17dd8f954003cb4ad33373c1be
kunzhi-yu <yukunzhi@hotmail.com>
2022-11-16 23:49:00 -0500

    Began writing the json read and write for users

--- src/main/java/DataAccess/UserDataAccess.java
+++ src/main/java/DataAccess/UserDataAccess.java
@@ -1,4 +1,28 @@
 package DataAccess;
+import org.*;
+import java.io.FileWriter;
 
-public class UserDataAccess {
+import DataAccessInterface.UserDataInterface;
+
+public class UserDataAccess implements UserDataInterface {
+    private final JSONObject jsonFile;
+    private final map
+
+    public void FileUserRead(String jsonPath) {
+        jsonFile = new JSONObject(jsonPath);
+        if (jsonFile.length() == 0) {
+            this.FileUserSave();
+
+        } else {
+
+        }
+    }
+
+    public void FileUserSave(String jsonPath) {
+
+    }
+
+    private void FileUserSave() {
+
+    }
 }
