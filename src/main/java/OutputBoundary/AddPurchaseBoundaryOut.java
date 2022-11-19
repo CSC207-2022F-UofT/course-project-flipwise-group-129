e27c2ca9e97787d9452339c7f8a0bdcb13e9267c
Aviraj Newatia <projectavi.exe@gmail.com>
2022-11-19 18:20:16 -0500

    Documented and commented the add purchase output boundary

--- src/main/java/OutputBoundary/AddPurchaseBoundaryOut.java
+++ src/main/java/OutputBoundary/AddPurchaseBoundaryOut.java
@@ -3,6 +3,7 @@ package OutputBoundary;
 import DataStructures.UpdatedLists;
 
 public interface AddPurchaseBoundaryOut {
-
+    // Defining the functions that will be implemented by the AddPurchase presenter in the output boundary so that the use case may call the presenter and update the view without violating clean architecture
     public void updateView(UpdatedLists updatedLists);
+    // This function uses the updated list data structure as a parameter as the new planning and purchase list are what will be updated on the view after add purchase is called
 }
