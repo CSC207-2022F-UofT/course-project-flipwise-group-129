1fe60fac93c447ffb398f5e880280dd6bd8defc4
Aviraj Newatia <projectavi.exe@gmail.com>
2022-11-19 17:34:28 -0500

    Updated the updatedLists data structure

--- src/main/java/DataStructures/UpdatedLists.java
+++ src/main/java/DataStructures/UpdatedLists.java
@@ -3,19 +3,19 @@ package DataStructures;
 import java.util.List;
 
 public class UpdatedLists {
-    private final List<String> newPlanningList;
-    private final List<String> newPurchasedList;
+    private final List<List<String>> newPlanningList;
+    private final List<List<String>> newPurchasedList;
 
-    public UpdatedLists(List<String> updatedPlanning, List<String> updatedPurchase) {
+    public UpdatedLists(List<List<String>> updatedPlanning, List<List<String>> updatedPurchase) {
         this.newPlanningList = updatedPlanning;
         this.newPurchasedList = updatedPurchase;
     }
 
-    public List<String> getNewPlanningList(){
+    public List<List<String>> getNewPlanningList(){
         return this.newPlanningList;
     }
 
-    public List<String> getNewPurchasedList(){
+    public List<List<String>> getNewPurchasedList(){
         return this.newPurchasedList;
     }
 }
