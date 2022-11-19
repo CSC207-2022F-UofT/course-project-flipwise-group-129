57114c64c01acb9ffebda10a1580d03cab86160e
Aviraj Newatia <projectavi.exe@gmail.com>
2022-11-19 18:02:19 -0500

    Documented the code for the AddPurchase controller

--- src/main/java/Controllers/AddPurchaseController.java
+++ src/main/java/Controllers/AddPurchaseController.java
@@ -11,12 +11,18 @@ import Presenters.AddPurchasePresenter;
 import java.util.List;
 
 public class AddPurchaseController {
+    // Defining the class attribute variable which will store the use case object as one of the boundary interface
     private AddPurchaseBoundaryIn addPurchaseUseCaseBoundaryIn;
 
-    public void controlAddPurchaseUseCase(String purchasedItemId, List<String> participatingUsernames, String buyerUsername, float price, String purchaseGroupId, GroupDataInterface groupDataAccess, ItemDataInterface itemDataAccess, UserDataInterface userDataAccess) {
+    public void controlAddPurchaseUseCase(AddPurchaseBoundaryIn addPurchaseUseCase, String purchasedItemId, List<String> participatingUsernames, String buyerUsername, float price, String purchaseGroupId, GroupDataInterface groupDataAccess, ItemDataInterface itemDataAccess, UserDataInterface userDataAccess) {
+        //Implement the instantiation of the presenter to pass along the pipeline and assign the boundary attribute variable to the use case object
+        this.addPurchaseUseCaseBoundaryIn = addPurchaseUseCase;
         AddPurchaseBoundaryOut presenter = new AddPurchasePresenter();
+
+        //Package the information into the data structure to pass to the use case through the boundary
         PurchaseInfo purchaseInfo = new PurchaseInfo(purchasedItemId, participatingUsernames, buyerUsername, price, purchaseGroupId, presenter, groupDataAccess, itemDataAccess, userDataAccess);
 
-        addPurchaseUseCaseBoundaryIn.executeUseCase(purchaseInfo);
+        // Call the use case
+        this.addPurchaseUseCaseBoundaryIn.executeUseCase(purchaseInfo);
     }
 }
