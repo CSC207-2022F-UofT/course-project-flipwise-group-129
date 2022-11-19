d243b98e2803d70809599ed7f94b286dfbaf2eef
sopleee <leesophie99@gmail.com>
2022-11-19 18:16:12 -0500

    completed changing output values for use case class

--- src/main/java/UseCases/AddToPlanningList.java
+++ src/main/java/UseCases/AddToPlanningList.java
@@ -6,6 +6,10 @@ import InputBoundary.AddToPlanningBoundaryIn;
 import DataStructures.UpdatedLists;
 import OutputBoundary.AddToPlanningBoundaryOut;
 import DataAccessInterface.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddToPlanningList implements AddToPlanningBoundaryIn{
     AddToPlanningBoundaryOut outputBoundary;
 
@@ -20,11 +24,34 @@ public class AddToPlanningList implements AddToPlanningBoundaryIn{
         Item newItem = createItem(item);
         currGroup.getPlanningList().addItems(newItem);
         saveGroup(groupId, groupAccess);
-        UpdatedLists updatedLists = new UpdatedLists(currGroup.getPlanningList(), currGroup.getPurchaseList());
+        UpdatedLists updatedLists = new UpdatedLists(
+                getUpdatedPlanning(currGroup.getPlanningList()), getUpdatedPurchase(currGroup.getPurchaseList()));
         // how detailed do you want retrieved from updated lists <-- all the info for items, I guess?
         return outputBoundary.displayLists(updatedLists);
     }
+    private List<List<String>> getUpdatedPurchase(PlanningList planningList){
+        List<List<String>> stringPlanningList = new ArrayList<>();
+        for(Item curItem: planningList.getItems()){
+            List<String> currentItem = new ArrayList<>();
+            currentItem.add(curItem.getItemId());
+            currentItem.add(curItem.getItemName());
+            stringPlanningList.add(currentItem);
+        }
+        return stringPlanningList;
+    }
 
+    private List<List<String>> getUpdatedPlanning(PurchaseList purchaseList){
+        List<List<String>> stringPurchasedList = new ArrayList<>();
+        for(Item curItem: purchaseList.getItems()){
+            List<String> currentItem = new ArrayList<>();
+            currentItem.add(curItem.getItemId());
+            currentItem.add(curItem.getItemName());
+            currentItem.add(curItem.getBuyer().getUsername());
+            currentItem.add(curItem.getPrice().toString());
+            stringPurchasedList.add(currentItem);
+        }
+        return stringPurchasedList;
+    }
     private Item createItem(PlannedItemInfo item){
         String itemId = ""; // UTC timestamp
         Item newItem = new Item(itemId, item.getName(), null , item.getPrice());
