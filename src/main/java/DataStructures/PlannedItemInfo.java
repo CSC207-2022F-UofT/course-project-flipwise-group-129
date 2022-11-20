e95b852b15cb5fa8b640a90bd444a2700b311bc9
sopleee <leesophie99@gmail.com>
2022-11-20 15:18:12 -0500

    added javadoc and handled errors to send message to the view

--- src/main/java/DataStructures/PlannedItemInfo.java
+++ src/main/java/DataStructures/PlannedItemInfo.java
@@ -5,13 +5,34 @@ public class PlannedItemInfo {
     final private Float price;
     final private String groupId;
 
+    /**
+     * Creates a new package of information needed to execute the AddtoPlanning use case which consists of
+     * information on the new item to add and its group
+     * @param name the new item's name
+     * @param price the new item's price
+     * @param groupId the groupId of the group storing this new item
+     */
     public PlannedItemInfo(String name, Float price, String groupId){
         this.name = name;
         this.price = price;
         this.groupId = groupId;
     }
 
+    /**
+     * This function retrieves the name of the item in the package
+     * @return This returns the name of the item in the package
+     */
     public String getName(){    return name;    }
+
+    /**
+     * This function retrieves the price of the item in the package
+     * @return This returns the price of the item in the package
+     */
     public Float getPrice(){    return price;   }
+
+    /**
+     * This function retrieves the groupId of the group of the item in the package
+     * @return This returns the groupId of the group of the item in the package
+     */
     public String getGroupId(){ return groupId; }
 }
--- src/main/java/UseCases/AddToPlanningList.java
+++ src/main/java/UseCases/AddToPlanningList.java
@@ -15,25 +15,54 @@ public class AddToPlanningList implements AddToPlanningBoundaryIn{
     GroupDataInterface groupAccess;
     ItemDataInterface itemAccess;
 
+    /**
+     * Creates a new AddtoPlanningList use case instance to implement the use case
+     * @param outputBoundary the output boundary to communicate updated display information to the view
+     * @param groupAccess the data access interface to retrieve and update group relevant data
+     * @param itemAccess the data access interface to update item relevant data
+     */
     public AddToPlanningList(
             AddToPlanningBoundaryOut outputBoundary, GroupDataInterface groupAccess, ItemDataInterface itemAccess){
         this.outputBoundary = outputBoundary;
         this.groupAccess = groupAccess;
         this.itemAccess = itemAccess;
     }
+
+    /**
+     * This function will be called from the controller to execute the use case AddtoPlanningList
+     * This function retrieves the current group object and creates a new item instance and adds
+     * to the group's planning list. It then saves the item and group back into the database
+     * @param item a package of all relevant information to create the item and access database
+     * @return This returns an instance of UpdatedLists data structure that contains lists of planning list and
+     * purchased list information, or if there is an error in the use case, a string that describes the error
+     */
     @Override
     public UpdatedLists addPlanning(PlannedItemInfo item) {
         String groupId = item.getGroupId();
         // Get the Group and Item entities to manipulate
         Group currGroup = retreiveGroup(groupId);
+        if (currGroup == null){
+            return outputBoundary.failErrorMessage(new UpdatedLists("Group cannot be found"));
+        }
         Item newItem = createItem(item);
+        if (newItem == null){
+            return outputBoundary.failErrorMessage(new UpdatedLists("Item could not be created"));
+        }
         // Add the new item to the current group's planning list and save it back into the database.
         currGroup.getPlanningList().addItems(newItem);
-        saveGroup(groupId, currGroup.toString());
+        if (!saveGroup(groupId, currGroup.toString())){
+            return outputBoundary.failErrorMessage(new UpdatedLists("PlanningList could not be updated"));
+        }
         UpdatedLists updatedLists = new UpdatedLists(
                 getUpdatedPlanning(currGroup.getPlanningList()), getUpdatedPurchase(currGroup.getPurchaseList()));
         return outputBoundary.displayLists(updatedLists);
     }
+
+    /**
+     * This function converts the current group's planning list object into a list that is readable in the view
+     * @param planningList the current group's planningList object
+     * @return This returns a list of itemName and itemId from the items in the current group's planning list
+     */
     private List<List<String>> getUpdatedPlanning(PlanningList planningList){
         List<List<String>> stringPlanningList = new ArrayList<>();
         for(Item curItem: planningList.getItems()){
@@ -44,7 +73,11 @@ public class AddToPlanningList implements AddToPlanningBoundaryIn{
         }
         return stringPlanningList;
     }
-
+    /**
+     * This function converts the current group's purchased list object into a list that is readable in the view
+     * @param purchaseList the current group's purchasedList object
+     * @return This returns a list of relevant information from the items in the current group's purchased list
+     */
     private List<List<String>> getUpdatedPurchase(PurchaseList purchaseList){
         List<List<String>> stringPurchasedList = new ArrayList<>();
         for(Item curItem: purchaseList.getItems()){
@@ -57,29 +90,45 @@ public class AddToPlanningList implements AddToPlanningBoundaryIn{
         }
         return stringPurchasedList;
     }
+    /**
+     * This function creates an item and saves the new item into the database
+     * @param item packaged information from the controller containing relevant data on the new item
+     * @return This returns the new item object created from the information or null if the item could not be saved
+     */
     private Item createItem(PlannedItemInfo item) {
         Item newItem = new Item(item.getName(), null , item.getPrice(), new ArrayList<>());
         try {
             itemAccess.addorUpdateItem(newItem.getItemId(), newItem.toString());
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            return null;
         }
         return newItem;
     }
+    /**
+     * This function returns a group object from the matching groupId in the database
+     * @param groupId the current group's groupId
+     * @return This returns a group object if found in the database, otherwise, null
+     */
     private Group retreiveGroup(String groupId) {
         String groupInfo = groupAccess.groupAsString(groupId);
         try {
             return Group.fromString(groupInfo);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            return null;
         }
     }
-
-    private void saveGroup(String groupId, String groupData) {
+    /**
+     * This function saves the updated group information back into the database
+     * @param groupId the current groupId
+     * @param groupData the current group's information
+     * @return This returns a success or failure of this attempt
+     */
+    private boolean saveGroup(String groupId, String groupData) {
         try {
             groupAccess.addorUpdateGroup(groupId, groupData);
+            return true;
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            return false;
         }
     }
 }
