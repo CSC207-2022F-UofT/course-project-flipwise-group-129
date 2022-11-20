3a483d8037edb0c0625f4a812c3c401bed6eacaa
Aviraj Newatia <projectavi.exe@gmail.com>
2022-11-19 21:08:21 -0500

    added javadocs to the output boundary

--- src/main/java/OutputBoundary/AddPurchaseBoundaryOut.java
+++ src/main/java/OutputBoundary/AddPurchaseBoundaryOut.java
@@ -7,6 +7,12 @@ import java.util.List;
 
 public interface AddPurchaseBoundaryOut {
     // Defining the functions that will be implemented by the AddPurchase presenter in the output boundary so that the use case may call the presenter and update the view without violating clean architecture
+
+    /**
+     * Prepares and returns the information changed by the use case to the view
+     * @param updatedLists the data structure with the new planning and purchased lists
+     * @return a hashmap mapping the name of each list to the actual list
+     */
     public HashMap<String, List<List<String>>> prepareViewInformation(UpdatedLists updatedLists);
     // This function uses the updated list data structure as a parameter as the new planning and purchase list are what will be updated on the view after add purchase is called
 }
