c7afe46c0da2bcffa1a20afae34a59dc6660d2e5
Aviraj Newatia <projectavi.exe@gmail.com>
2022-11-19 20:47:04 -0500

    Added javadocs for the functions in updatedlists data structure

--- src/main/java/DataStructures/UpdatedLists.java
+++ src/main/java/DataStructures/UpdatedLists.java
@@ -7,6 +7,11 @@ public class UpdatedLists {
     private final List<List<String>> newPlanningList;
     private final List<List<String>> newPurchasedList;
 
+    /**
+     * Assigns values from the parameters to the class attribute variables
+     * @param updatedPlanning the new 2 dimensional list of the planning list of the group
+     * @param updatedPurchase the new 2 dimensional list of the purchased list of the group
+     */
     public UpdatedLists(List<List<String>> updatedPlanning, List<List<String>> updatedPurchase) {
         // Instantiate the class attributes from the information returned from the use case
         this.newPlanningList = updatedPlanning;
@@ -14,10 +19,17 @@ public class UpdatedLists {
     }
 
     // Implementing getter functions for the new planning and purchased lists so that the presenter can use them to update the view
+
+    /**
+     * @return the private new planning list attribute
+     */
     public List<List<String>> getNewPlanningList(){
         return this.newPlanningList;
     }
 
+    /**
+     * @return the private new purchased list attribute
+     */
     public List<List<String>> getNewPurchasedList(){
         return this.newPurchasedList;
     }
