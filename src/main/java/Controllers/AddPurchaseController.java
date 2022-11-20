9f7c4b4a7e1df32667ddd70342fe736ad00d0e4c
Aviraj Newatia <projectavi.exe@gmail.com>
2022-11-19 21:36:14 -0500

    Update AddPurchaseController.java

--- src/main/java/Controllers/AddPurchaseController.java
+++ src/main/java/Controllers/AddPurchaseController.java
@@ -19,15 +19,15 @@ public class AddPurchaseController {
      * from a group purchases an item from the planning list. This controller function packages all the required information that
      * the view sends forward into the PurchaseInfo data structure, instantiates the presenter and passes all of this information
      * to call the use case through the input boundary.
-     * @param addPurchaseUseCase
-     * @param purchasedItemId
-     * @param participatingUsernames
-     * @param buyerUsername
-     * @param price
-     * @param purchaseGroupId
-     * @param groupDataAccess
-     * @param itemDataAccess
-     * @param userDataAccess
+     * @param addPurchaseUseCase object of the use case
+     * @param purchasedItemId item id of the item being purchased
+     * @param participatingUsernames usernames of the users involved in the purchase
+     * @param buyerUsername username of the user buying the item
+     * @param price price of the item
+     * @param purchaseGroupId id of the group the purchase is made in
+     * @param groupDataAccess interface for group data access
+     * @param itemDataAccess interface for item data access
+     * @param userDataAccess interface for user data access
      */
     public void controlAddPurchaseUseCase(AddPurchaseBoundaryIn addPurchaseUseCase, String purchasedItemId, List<String> participatingUsernames, String buyerUsername, float price, String purchaseGroupId, GroupDataInterface groupDataAccess, ItemDataInterface itemDataAccess, UserDataInterface userDataAccess) {
         //Implement the instantiation of the presenter to pass along the pipeline and assign the boundary attribute variable to the use case object
