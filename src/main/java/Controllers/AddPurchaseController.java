0b85cb06549186b9d9123458a705b1e0a4fab24c
Aviraj Newatia <projectavi.exe@gmail.com>
2022-11-19 20:26:32 -0500

    Added a javadoc for the controlAddPurchaseUseCase function in the controller

--- src/main/java/Controllers/AddPurchaseController.java
+++ src/main/java/Controllers/AddPurchaseController.java
@@ -14,6 +14,21 @@ public class AddPurchaseController {
     // Defining the class attribute variable which will store the use case object as one of the boundary interface
     private AddPurchaseBoundaryIn addPurchaseUseCaseBoundaryIn;
 
+    /**
+     * This function will be called from the main/view in order to control the use case AddPurchase which occurs whenever a user
+     * from a group purchases an item from the planning list. This controller function packages all the required information that
+     * the view sends forward into the PurchaseInfo data structure, instantiates the presenter and passes all of this information
+     * to call the use case through the input boundary.
+     * @param addPurchaseUseCase
+     * @param purchasedItemId
+     * @param participatingUsernames
+     * @param buyerUsername
+     * @param price
+     * @param purchaseGroupId
+     * @param groupDataAccess
+     * @param itemDataAccess
+     * @param userDataAccess
+     */
     public void controlAddPurchaseUseCase(AddPurchaseBoundaryIn addPurchaseUseCase, String purchasedItemId, List<String> participatingUsernames, String buyerUsername, float price, String purchaseGroupId, GroupDataInterface groupDataAccess, ItemDataInterface itemDataAccess, UserDataInterface userDataAccess) {
         //Implement the instantiation of the presenter to pass along the pipeline and assign the boundary attribute variable to the use case object
         this.addPurchaseUseCaseBoundaryIn = addPurchaseUseCase;
