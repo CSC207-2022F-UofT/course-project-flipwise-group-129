8afad20ae98d41d66eb8b2ea3301455c52ecdce2
kunzhi-yu <yukunzhi@hotmail.com>
2022-11-21 23:23:00 -0500

    fixed intelliji warnings

--- src/main/java/DataStructures/LoggedInInfo.java
+++ src/main/java/DataStructures/LoggedInInfo.java
@@ -6,10 +6,6 @@ package DataStructures;
 import java.util.List;
 
 public class LoggedInInfo {
-    private boolean statusBool = true;
-    private String username;
-
-    private List<List<Object>> userAllGroups;
 
     /**
      * Constructor called when the login was successful. User data is passed through this data structure.
@@ -18,8 +14,6 @@ public class LoggedInInfo {
      * @param allGroups the groups a user is in and the groups associated details
      */
     public LoggedInInfo(String username, List<List<Object>> allGroups) {
-        this.username = username;
-        this.userAllGroups = allGroups;
     }
 
     /**
@@ -27,7 +21,6 @@ public class LoggedInInfo {
      * @param failed to report the login as failed
      */
     public LoggedInInfo(boolean failed) {
-        this.statusBool = failed;
     }
 
 }
