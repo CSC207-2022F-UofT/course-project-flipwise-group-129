254dbd77f24ad9a56577518c9ad0bc221996278a
sopleee <leesophie99@gmail.com>
2022-11-25 23:29:13 -0500

    finished data testing for group database

--- src/test/java/UseCases/AddToPlanningListTest.java
+++ src/test/java/UseCases/AddToPlanningListTest.java
@@ -43,7 +43,7 @@ class AddToPlanningListTest {
 
         // 2) Input data — we can make this up for the test. Normally it would
         // be created by the Controller.
-        PlannedItemInfo newItem = new PlannedItemInfo("Maggi", "group1");
+        PlannedItemInfo newItem = new PlannedItemInfo("maggi", "group1");
 
         // 3) Run the use case
         UpdatedLists outputData = usecase.addPlanning(newItem);
@@ -82,7 +82,7 @@ class AddToPlanningListTest {
 
         // 2) Input data — we can make this up for the test. Normally it would
         // be created by the Controller.
-        PlannedItemInfo newItem = new PlannedItemInfo("Maggi", "noGroup");
+        PlannedItemInfo newItem = new PlannedItemInfo("maggi", "noGroup");
 
         // 3) Run the use case
         UpdatedLists outputData = usecase.addPlanning(newItem);
@@ -95,8 +95,7 @@ class AddToPlanningListTest {
     }
 
     @Test
-    void itemAddedSuccessDatabaseUpdateCheck(){
-        boolean flagExists = false;
+    void createItemDbCheck() throws IOException, org.json.simple.parser.ParseException {
         GroupDataInterface groupData = null;
         ItemDataInterface itemData = null;
         {
@@ -113,68 +112,56 @@ class AddToPlanningListTest {
 
         // 2) Input data — we can make this up for the test. Normally it would
         // be created by the Controller.
-        PlannedItemInfo newItem = new PlannedItemInfo("Maggi", "noGroup");
-
+        PlannedItemInfo newItem = new PlannedItemInfo("maggi", "noGroup");
+        List<String> itemInfoBefore = getItemInfo();
+        Group groupBefore = getGroupInfo();
         // 3) Run the use case
-        UpdatedLists outputData = usecase.addPlanning(newItem);
-
-        if(outputData.getNewPlanningList() == null && outputData.getResultMessage().equals("Group cannot be found")){
-            flagExists = true;
-        }
-
-        assert (flagExists);
-    }
-
-    @Test
-    void createUserDbCheck() throws IOException, org.json.simple.parser.ParseException {
-        GroupDataInterface groupData = null;
-        ItemDataInterface itemData = null;
-        {
-            try {
-                groupData = new GroupDataAccess();
-                itemData = new ItemDataAccess();
-            } catch (IOException | ParseException e) {
-                assert(false);
-            }
-        }
-
-        AddToPlanningPresenter presenter = new AddToPlanningPresenter();
-        AddToPlanningBoundaryIn usecase = new AddToPlanningList(presenter, groupData, itemData);
-
-        // 2) Input data — we can make this up for the test. Normally it would
-        // be created by the Controller.
-        PlannedItemInfo newItem = new PlannedItemInfo("Maggi", "noGroup");
-        String userInfoBefore = getItemInfo();
-        // 3) Run the use case
-        UpdatedLists outputData = usecase.addPlanning(newItem);
-        // chekc if the users list of groups has been updated
+        usecase.addPlanning(newItem);
         try {
-            String userInfoAfter = getItemInfo();
-            assert userInfoBefore.equals("itemId does not exist");
-            assert userInfoAfter.equals("maggi");
+            List<String> itemInfoAfter = getItemInfo();
+            Group groupAfter = getGroupInfo();
+            assert !(groupAfter == null | groupBefore == null);
+            assert (groupBefore.getPlanningList().getItems().size() + 1) == groupAfter.getPlanningList().getItems().size();
+            assert itemInfoBefore.get(0).equals("itemId does not exist");
+            assert itemInfoAfter.get(1).equals("maggi");
         } catch (IOException | ParseException e) {
             throw new RuntimeException(e);
         }
-
-        // confirm now that the group is in the db
-        assert groupData.groupIdExists("0001");
-
     }
 
-    String getItemInfo() throws IOException, ParseException {
+    List<String> getItemInfo() throws IOException, ParseException {
         ItemDataInterface itemDsInterface = new ItemDataAccess();
+        List<String> itemInfo = new ArrayList<>();
         Map<String, String> itemDsMap = itemDsInterface.getItemMap();
-        String currentItemId = null;
         for(Map.Entry<String, String> curItem: itemDsMap.entrySet()){
             try {
                 String curItemName = Item.fromString(curItem.getValue()).getItemName();
                 if(curItemName.equals("maggi")){
-                    return curItem.getKey();
+                    itemInfo.add(curItem.getKey());
+                    itemInfo.add(curItemName);
+                    return itemInfo;
                 }
             } catch (JsonProcessingException e) {
-                return "Unable to process item from the database";
+                itemInfo.add("Unable to process item from the database");
+                return itemInfo;
             }
         }
-        return "item does not exist";
+        itemInfo.add("item does not exist");
+        return itemInfo;
+    }
+    Group getGroupInfo() throws IOException, ParseException {
+        GroupDataInterface groupDsInterface = new GroupDataAccess();
+        Map<String, String> groupDsMap = groupDsInterface.getGroupMap();
+        for(Map.Entry<String, String> curGroup: groupDsMap.entrySet()){
+            try {
+                Group curGroupEntity = Group.fromString(curGroup.getValue());
+                if(curGroupEntity.getGroupName().equals("group1")){
+                    return curGroupEntity;
+                }
+            } catch (JsonProcessingException e) {
+                return null;
+            }
+        }
+        return null;
     }
 }
