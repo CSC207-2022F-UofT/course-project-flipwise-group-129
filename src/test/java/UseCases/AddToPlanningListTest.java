b9ca4428c4788cb1d34b0fdf36d520e16736f048
sopleee <leesophie99@gmail.com>
2022-11-25 21:59:56 -0500

    began writing database tests

--- src/test/java/UseCases/AddToPlanningListTest.java
+++ src/test/java/UseCases/AddToPlanningListTest.java
@@ -12,41 +12,33 @@ import DataStructures.PlannedItemInfo;
 import DataStructures.UpdatedLists;
 import Entities.*;
 import InputBoundary.AddToPlanningBoundaryIn;
-import UseCases.AddToPlanningList;
 import Presenters.AddToPlanningPresenter;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 class AddToPlanningListTest {
-    // To test the use case:
-    // 1) Create a GroupCreateInteractorTest and prerequisite objects
-    //    (arguments for the GroupCreateController constructor parameters)
-    // 2) create the Input Data in the form of the group and users
-    // 3) Call the use case GroupCreate Input Boundary method to run the use case
-    // 4) Check that the Output Data passed to the Presenter is correct
-    // 5) Check that the expected changes to the data layer are there.
-
     @Test
-    void createUserNotExist() throws IOException, ParseException {
-        GroupDataInterface groupData;
-        ItemDataInterface itemData;
+    void itemAddedWithAllRequiredInfoTest(){
+        boolean flagExists = false;
+        GroupDataInterface groupData = null;
+        ItemDataInterface itemData = null;
         {
             try {
                 groupData = new GroupDataAccess();
                 itemData = new ItemDataAccess();
             } catch (IOException | ParseException e) {
-                throw new RuntimeException(e);
+                assert(false);
             }
         }
 
         AddToPlanningPresenter presenter = new AddToPlanningPresenter();
         AddToPlanningBoundaryIn usecase = new AddToPlanningList(presenter, groupData, itemData);
-
-
 
         // 2) Input data — we can make this up for the test. Normally it would
         // be created by the Controller.
@@ -55,13 +47,130 @@ class AddToPlanningListTest {
         // 3) Run the use case
         UpdatedLists outputData = usecase.addPlanning(newItem);
 
-        boolean flagExists = false;
-        for (List<String> temp: outputData.getNewPlanningList()) {
-            if (Objects.equals(temp.get(0), "Maggi")) {
-                flagExists = true;
-                break;
+        if(outputData.getNewPlanningList() != null){
+            for (List<String> temp: outputData.getNewPlanningList()) {
+                if (Objects.equals(temp.get(1), "Maggi")) {
+                    flagExists = true;
+                    break;
+                }
             }
+        }
+        else {
+            assert(false);
         }
 
         assert (flagExists);
-}}
+    }
+
+    @Test
+    void itemNotAddedIfGroupIdDNETest(){
+        boolean flagExists = false;
+        GroupDataInterface groupData = null;
+        ItemDataInterface itemData = null;
+        {
+            try {
+                groupData = new GroupDataAccess();
+                itemData = new ItemDataAccess();
+            } catch (IOException | ParseException e) {
+                assert(false);
+            }
+        }
+
+        AddToPlanningPresenter presenter = new AddToPlanningPresenter();
+        AddToPlanningBoundaryIn usecase = new AddToPlanningList(presenter, groupData, itemData);
+
+        // 2) Input data — we can make this up for the test. Normally it would
+        // be created by the Controller.
+        PlannedItemInfo newItem = new PlannedItemInfo("Maggi", "noGroup");
+
+        // 3) Run the use case
+        UpdatedLists outputData = usecase.addPlanning(newItem);
+
+        if(outputData.getNewPlanningList() == null && outputData.getResultMessage().equals("Group cannot be found")){
+            flagExists = true;
+        }
+
+        assert (flagExists);
+    }
+
+    @Test
+    void itemAddedSuccessDatabaseUpdateCheck(){
+        boolean flagExists = false;
+        GroupDataInterface groupData = null;
+        ItemDataInterface itemData = null;
+        {
+            try {
+                groupData = new GroupDataAccess();
+                itemData = new ItemDataAccess();
+            } catch (IOException | ParseException e) {
+                assert(false);
+            }
+        }
+
+        AddToPlanningPresenter presenter = new AddToPlanningPresenter();
+        AddToPlanningBoundaryIn usecase = new AddToPlanningList(presenter, groupData, itemData);
+
+        // 2) Input data — we can make this up for the test. Normally it would
+        // be created by the Controller.
+        PlannedItemInfo newItem = new PlannedItemInfo("Maggi", "noGroup");
+
+        // 3) Run the use case
+        UpdatedLists outputData = usecase.addPlanning(newItem);
+
+        if(outputData.getNewPlanningList() == null && outputData.getResultMessage().equals("Group cannot be found")){
+            flagExists = true;
+        }
+
+        assert (flagExists);
+    }
+
+    @Test
+    void createUserDbCheck() throws IOException, org.json.simple.parser.ParseException {
+        GroupDataInterface groupData = null;
+        ItemDataInterface itemData = null;
+        {
+            try {
+                groupData = new GroupDataAccess();
+                itemData = new ItemDataAccess();
+            } catch (IOException | ParseException e) {
+                assert(false);
+            }
+        }
+
+        AddToPlanningPresenter presenter = new AddToPlanningPresenter();
+        AddToPlanningBoundaryIn usecase = new AddToPlanningList(presenter, groupData, itemData);
+
+        // 2) Input data — we can make this up for the test. Normally it would
+        // be created by the Controller.
+        PlannedItemInfo newItem = new PlannedItemInfo("Maggi", "noGroup");
+        String userInfoBefore = getItemInfo();
+        // 3) Run the use case
+        UpdatedLists outputData = usecase.addPlanning(newItem);
+        // chekc if the users list of groups has been updated
+        try {
+            String userInfoAfter = getItemInfo();
+            assert userInfoBefore.equals("itemId does not exist");
+            assert userInfoAfter.equals("maggi");
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        // confirm now that the group is in the db
+        assert groupData.groupIdExists("0001");
+
+    }
+
+    String getItemInfo() throws IOException, ParseException {
+        ItemDataInterface itemDsInterface = new ItemDataAccess();
+        if (!itemDsInterface.itemIdExists("")){
+            return "itemId does not exist";
+        }
+        try {
+            String itemId = "";
+            Item retrievedItem = Item.fromString(itemDsInterface.itemAsString(itemId));
+            return retrievedItem.getItemName();
+        } catch (JsonProcessingException e) {
+            return "Unable to process item from the database";
+        }
+    }
+}
