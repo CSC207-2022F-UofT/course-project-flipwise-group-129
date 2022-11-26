5ee48d3e807c620d00aef640935ea6475bb147f3
Aviraj Newatia <projectavi.exe@gmail.com>
2022-11-25 21:26:50 -0500

    Added database test

--- src/test/java/add_purchase_use_case/AddPurchaseControllerTest.java
+++ src/test/java/add_purchase_use_case/AddPurchaseControllerTest.java
@@ -8,9 +8,13 @@ import DataAccessInterface.ItemDataInterface;
 import DataAccessInterface.UserDataInterface;
 import DataStructures.PurchaseInfo;
 import DataStructures.UpdatedLists;
+import Entities.Group;
 import Entities.Item;
+import Entities.PlanningList;
+import Entities.PurchaseList;
 import InputBoundary.AddPurchaseBoundaryIn;
 import UseCases.AddPurchase;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 import Presenters.AddPurchasePresenter;
@@ -24,7 +28,7 @@ import java.util.Objects;
 class AddPurchaseControllerTest {
 
     @Test
-    void create() throws IOException, ParseException {
+    void testPurchaseSuccess() throws IOException, ParseException {
         // To test the use case:
         // 1) Create a AddPurchaseController and prerequisite objects
         //    (arguments for the AddPurchaseController constructor parameters)
@@ -65,5 +69,64 @@ class AddPurchaseControllerTest {
 
         Item finalItem = Item.fromString(itemData.itemAsString("1"));
         assert(finalItem.getPrice() != 0.0 && finalItem.getBuyer() != null);
+
+    }
+
+    @Test
+    void updateDbTest() throws IOException, ParseException {
+        // 1) Instantiate
+        AddPurchasePresenter presenter = new AddPurchasePresenter();
+        AddPurchaseBoundaryIn usecase = new AddPurchase();
+        GroupDataInterface groupData = new GroupDataAccess();
+        UserDataInterface userData = new UserDataAccess();
+        ItemDataInterface itemData = new ItemDataAccess();
+
+
+        // 2) Input data — we can make this up for the test. Normally it would
+        // be created by the Controller.
+        PurchaseInfo inputData = new PurchaseInfo("1", new ArrayList<>(Collections.singleton("Avi")),
+                "Avi", 10.0f, "group1", presenter, groupData, itemData, userData);
+
+        // 3) Run the use case
+        UpdatedLists outputData = usecase.executeUseCase(inputData);
+
+        Group groupInfoAfter = getGroupInfo();
+
+        List<List<String>> returnedPlanning = outputData.getNewPlanningList();
+        List<List<String>> returnedPurchased = outputData.getNewPurchasedList();
+
+        PlanningList dbPlanning = groupInfoAfter.getPlanningList();
+        PurchaseList dbPurchased = groupInfoAfter.getPurchaseList();
+
+        assert (dbPlanning.getItems().size() == returnedPlanning.size());
+        assert (dbPurchased.getItems().size() == returnedPurchased.size());
+
+        for (int i = 0; i < dbPlanning.getItems().size(); i++) {
+            assert (Objects.equals(dbPlanning.getItems().get(i).getItemId(), returnedPlanning.get(i).get(0)));
+            assert (Objects.equals(dbPlanning.getItems().get(i).getItemName(), returnedPlanning.get(i).get(1)));
+        }
+
+        for (int i = 0; i < dbPurchased.getItems().size(); i++) {
+            assert (Objects.equals(dbPurchased.getItems().get(i).getItemId(), returnedPurchased.get(i).get(0)));
+            assert (Objects.equals(dbPurchased.getItems().get(i).getItemName(), returnedPurchased.get(i).get(1)));
+            assert (Objects.equals(String.valueOf(dbPurchased.getItems().get(i).getPrice()), returnedPurchased.get(i).get(2)));
+            assert (Objects.equals(dbPurchased.getItems().get(i).getBuyer().getUsername(), returnedPurchased.get(i).get(3)));
+        }
+    }
+
+    Group getGroupInfo() throws IOException, ParseException {
+        GroupDataInterface groupDsInterface = new GroupDataAccess();
+        List<String> stringUsers = new ArrayList<>();
+        List<String> stringItems = new ArrayList<>();
+        List<String> stringPlanned = new ArrayList<>();
+        // get the user from the database
+        //check if the user exists
+        if (!groupDsInterface.groupIdExists("group1")){
+            throw new RuntimeException("Group Id does not exist");
+        }
+        String groupString = groupDsInterface.groupAsString("group1");
+
+        Group group = Group.fromString(groupString);
+        return group;
     }
 }
