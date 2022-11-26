76b5c552153bba80ae6334b2cccba938fdb07886
sopleee <leesophie99@gmail.com>
2022-11-26 16:44:06 -0500

    added iterator function in use case and changed error string values

--- src/main/java/UseCases/SettlementPayment.java
+++ src/main/java/UseCases/SettlementPayment.java
@@ -8,6 +8,7 @@ import OutputBoundary.SettlementBoundaryOut;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class SettlementPayment implements SettlementBoundaryIn{
@@ -38,11 +39,11 @@ public class SettlementPayment implements SettlementBoundaryIn{
     public UpdatedDebts executeDebtSettlement(PaymentDetails settlement) {
         String groupId = settlement.getGroupId();
         // Get the Group entity to manipulate
-        Group currGroup = null;
+        Group currGroup;
         try {
             currGroup = retreiveGroup(groupId);
         } catch (RuntimeException e) {
-            return raiseError(e.toString());
+            return raiseError("group unable to be retrieved from database");
         }
         // get the corresponding debt pair and set the value to 0.0.
         Debt currentDebt = currGroup.getPurchaseBalance().getDebtPair(settlement.getOwed(), settlement.getOwing());
@@ -50,7 +51,7 @@ public class SettlementPayment implements SettlementBoundaryIn{
         try {
             saveGroup(settlement.getGroupId(), currGroup.toString());
         } catch (RuntimeException e) {
-            return raiseError(e.toString());
+            return raiseError("unable to save debt changes");
         }
 
         List<List<String>> stringDebtList = getUpdatedDebts(currGroup.getPurchaseBalance());
@@ -65,17 +66,26 @@ public class SettlementPayment implements SettlementBoundaryIn{
     /**
      * This function gets a list of all the new debts in the group
      * @param purchaseBalance the list of Debts in the group
-     * @return This returns a list of debts formatted as a list of list of string [userOwedUsername, userOwingUsername, debtValue]
+     * @return This returns a list of debts formatted as a nested list of strings [userOwedUsername, userOwingUsername, debtValue]
      */
     private List<List<String>> getUpdatedDebts(PurchaseBalance purchaseBalance){
         List<List<String>> stringPurchaseBalance = new ArrayList<>();
-        for(Debt curDebt: purchaseBalance.getAllDebts()){
+        Iterator<String> iter = purchaseBalance.iterator();
+        while(iter.hasNext()){
+            Debt curDebt = iter.next();
             List<String> currentDebt = new ArrayList<>();
             currentDebt.add(curDebt.getUserOwed().getUsername());
             currentDebt.add(curDebt.getUserOwing().getUsername());
             currentDebt.add(curDebt.getDebtValue().toString());
             stringPurchaseBalance.add(currentDebt);
         }
+//        for(Debt curDebt: purchaseBalance.getAllDebts()){
+//            List<String> currentDebt = new ArrayList<>();
+//            currentDebt.add(curDebt.getUserOwed().getUsername());
+//            currentDebt.add(curDebt.getUserOwing().getUsername());
+//            currentDebt.add(curDebt.getDebtValue().toString());
+//            stringPurchaseBalance.add(currentDebt);
+//        }
         return stringPurchaseBalance;
     }
 
