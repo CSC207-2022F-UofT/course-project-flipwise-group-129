55455f180e3e14a562970e0a3aad59b1ba8f2cc1
rcordi <64288591+rcordi@users.noreply.github.com>
2022-11-27 17:22:29 -0500

    Fixed errors in ButtonTable and PlanningListView.

--- src/main/java/View/PlanningListView.java
+++ src/main/java/View/PlanningListView.java
@@ -25,69 +25,53 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.List;
 
-public class PlanningListView extends JPanel {
+public class PlanningListView extends JPanel implements ActionListener{
     private JTable table;
+    private String username;
+    private String groupID;
+    private List<String> groupUserName;
     private JScrollPane scrollPane;
     Object[][] rows;
-    String[] columns = new String[]{"", "Item name"};
+    String[] columns = new String[]{" ", "Item name"};
     GroupSummaryView groupSummaryView;
+
+    private ButtonTable buttonTable;
+    private final AddPurchaseController controllerAddPurchase;
 
     public PlanningListView(List<List<String>> planningListData, String username, String groupID,
                             List<String> groupUserName) {
+
+        this.username = username;
+        this.groupID = groupID;
+        this.groupUserName = groupUserName;
+
+        ItemDataInterface itemData;
+        GroupDataInterface groupData;
+        UserDataInterface userData;
+        try {
+            itemData = new ItemDataAccess();
+            groupData = new GroupDataAccess();
+            userData = new UserDataAccess();
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e); // Display popup
+        }
+
+        AddPurchaseBoundaryOut presenter = new AddPurchasePresenter();
+
+
+        AddPurchaseBoundaryIn useCase = new AddPurchase();
+
+        this.controllerAddPurchase = new AddPurchaseController(presenter, useCase, groupData, itemData, userData);
 
         setRows(planningListData);
         DefaultTableModel model = new DefaultTableModel(rows, columns);
         table = new JTable(model);
 
-        Action delete = new AbstractAction()
-        {
-            private AddPurchaseController controllerAddPurchase;
+        ButtonTable buttonTable = new ButtonTable(table);
+        table.getColumn(" ").setCellEditor(buttonTable);
+        table.getColumn(" ").setCellRenderer(buttonTable);
 
-            public void actionPerformed(ActionEvent evt)
-            {
-                JTable table = (JTable)evt.getSource();
-                int modelRow = Integer.valueOf( evt.getActionCommand() );
-
-                AddPurchaseView addPurchaseView = new AddPurchaseView(username, groupID, "PLACEHOLDER", groupUserName);
-                if (addPurchaseView.getReply() == JOptionPane.YES_OPTION) {
-
-
-                    ((DefaultTableModel)table.getModel()).removeRow(modelRow);
-                    JOptionPane.showMessageDialog(null, "Purchased item.");
-
-                    ItemDataInterface itemData;
-                    GroupDataInterface groupData;
-                    UserDataInterface userData;
-                    try {
-                        itemData = new ItemDataAccess();
-                        groupData = new GroupDataAccess();
-                        userData = new UserDataAccess();
-                    } catch (IOException | ParseException e) {
-                        throw new RuntimeException(e); // Display popup
-                    }
-
-                    AddPurchaseBoundaryOut presenter = new AddPurchasePresenter();
-
-
-                    AddPurchaseBoundaryIn useCase = new AddPurchase();
-
-                    this.controllerAddPurchase = new AddPurchaseController(presenter, useCase, groupData, itemData, userData);
-
-                    this.controllerAddPurchase.controlAddPurchaseUseCase("Item#23", addPurchaseView.getSelectedMembers(),
-                            "Saleh", Float.parseFloat(addPurchaseView.item_price.getText().toString()),
-                            "Group#24");
-                }
-
-                else {
-                    JOptionPane.showMessageDialog(null, "Did not purchase item.");
-                }
-
-            }
-        };
-
-//        ButtonColumn buttonColumn = new ButtonColumn(table, delete, 0);
-
-
+        buttonTable.getPurchase().addActionListener(this);
         scrollPane = new JScrollPane(table);
 
         add(scrollPane);
@@ -95,8 +79,22 @@ public class PlanningListView extends JPanel {
         setVisible(true);
         setSize(1000, 600);
 
+
     }
 
+    public void actionPerformed(ActionEvent evt) {
+        if (evt.getActionCommand().equals("Purchase")) {
+            AddPurchaseView addPurchaseView = new AddPurchaseView(this.username, this.groupID,
+                    "PLACEHOLDER", this.groupUserName);
+
+            JOptionPane.showMessageDialog(null, "Purchased item.");
+
+            this.controllerAddPurchase.controlAddPurchaseUseCase("Item#23", addPurchaseView.getSelectedMembers(),
+                    "Saleh", Float.parseFloat(addPurchaseView.item_price.getText().toString()),
+                    "Group#24");
+
+        }
+    }
 //    public void setPlanningList(){
 //        // filter through codis data (codis data in parameter)
 //        // get updated planning list and reload page
@@ -107,11 +105,13 @@ public class PlanningListView extends JPanel {
 //                {"Purchase", "8"}};
 //    }
 
-    public void setRows(List<List<String>> planningListData) {
-        this.rows = new Object[planningListData.size()][2];
-        for (int i = 0; i < planningListData.size(); i++) {
-            this.rows[i] = planningListData.get(i).toArray();
-            //CONFIRM WITH CODI WHAT IS IN THIS 2D ARRAY
+        public void setRows (List < List < String>> planningListData) {
+            this.rows = new Object[planningListData.size()][2];
+            for (int i = 0; i < planningListData.size(); i++) {
+                this.rows[i] = planningListData.get(i).toArray();
+                //CONFIRM WITH CODI WHAT IS IN THIS 2D ARRAY
+            }
         }
     }
-}
+
+
