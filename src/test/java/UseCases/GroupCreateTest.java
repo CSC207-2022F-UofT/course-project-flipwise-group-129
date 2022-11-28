7471714e450ea4a36b851502b17d3e2816d43cb2
rowanArora <mishaalhere@gmail.com>
2022-11-28 13:00:22 -0500

    creating and deleting temp db for testing

--- src/test/java/UseCases/GroupCreateTest.java
+++ src/test/java/UseCases/GroupCreateTest.java
@@ -6,15 +6,24 @@ import DataAccessInterface.GroupDataInterface;
 import DataAccessInterface.UserDataInterface;
 import DataStructures.CreatedGroupInfo;
 import DataStructures.ProposedGroupInfo;
+import Entities.Group;
 import Entities.User;
 import InputBoundary.GroupCreateBoundaryIn;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.json.simple.parser.ParseException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import Presenters.GroupCreatePresenter;
@@ -23,9 +32,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GroupCreateTest {
 
+    @Before
+    public void setUp() throws IOException {
+        //copy and create duplicate test stuff
+        Path copiedGroups = Paths.get("src/test/resources/testgroupsCopy.json");
+        Path originalPathGroups = Paths.get("src/test/resources/testgroups.json");
+        Files.copy(originalPathGroups, copiedGroups, StandardCopyOption.REPLACE_EXISTING);
+
+        Path copiedUsers = Paths.get("src/test/resources/testusersCopy.json");
+        Path originalPathUsers = Paths.get("src/test/resources/testusers.json");
+        Files.copy(originalPathUsers, copiedUsers, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    @After
+    public void tearDown(){
+        File groupFile = new File("src/test/resources/testgroupsCopy.json");
+        groupFile.delete();
+
+        File userFile = new File("src/test/resources/testusersCopy.json");
+        userFile.delete();
+    }
+
     List<String> getUserInfo() throws IOException, ParseException {
-        UserDataInterface userDsInterface = new UserDataAccess();
-        List<String> stringGroups = new ArrayList<>();
+        UserDataInterface userDsInterface = new UserDataAccess("src/test/resources/testusersCopy.json");
         // get the user from the database
         //check if the user exists
         if (!userDsInterface.userIdExists("mishaalk")){
@@ -34,8 +63,7 @@ class GroupCreateTest {
         String userString = userDsInterface.userAsString("mishaalk");
 
         try {
-            User.fromString(userString).getGroups().forEach(group -> stringGroups.add(group.getGroupId()));
-            return stringGroups;
+            return new ArrayList<>(User.fromString(userString).getGroups());
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Unable to process user from database");
         }
@@ -50,6 +78,8 @@ class GroupCreateTest {
         // 3) Call the use case GroupCreate Input Boundary method to run the use case
         // 4) Check that the Output Data passed to the Presenter is correct
         // 5) Check that the expected changes to the data layer are there.
+
+        setUp();
 
         // 1) Instantiate
         GroupCreatePresenter presenter = new GroupCreatePresenter() {
@@ -70,8 +100,8 @@ class GroupCreateTest {
         };
 
 
-        GroupDataInterface groupData = new GroupDataAccess();
-        UserDataInterface userData = new UserDataAccess();
+        GroupDataInterface groupData = new GroupDataAccess("src/test/resources/testgroups.json");
+        UserDataInterface userData = new UserDataAccess("src/test/resources/testusersCopy.json");
         GroupCreateBoundaryIn useCase = new GroupCreate(presenter, groupData, userData);
 
         // 2) Input data — we can make this up for the test. Normally it would
@@ -80,6 +110,8 @@ class GroupCreateTest {
 
         // 3) Run the use case
         useCase.createNewGroup(inputData);
+
+        tearDown();
     }
 
     @Test
@@ -91,6 +123,8 @@ class GroupCreateTest {
         // 3) Call the use case GroupCreate Input Boundary method to run the use case
         // 4) Check that the Output Data passed to the Presenter is correct
         // 5) Check that the expected changes to the data layer are there.
+
+        setUp();
 
         // 1) Instantiate
         GroupCreatePresenter presenter = new GroupCreatePresenter() {
@@ -113,8 +147,8 @@ class GroupCreateTest {
         };
 
 
-        GroupDataInterface groupData = new GroupDataAccess();
-        UserDataInterface userData = new UserDataAccess();
+        GroupDataInterface groupData = new GroupDataAccess("src/test/resources/testgroups.json");
+        UserDataInterface userData = new UserDataAccess("src/test/resources/testusersCopy.json");
         GroupCreateBoundaryIn useCase = new GroupCreate(presenter, groupData, userData);
 
         // 2) Input data — we can make this up for the test. Normally it would
@@ -123,6 +157,8 @@ class GroupCreateTest {
 
         // 3) Run the use case
         useCase.createNewGroup(inputData);
+
+        tearDown();
     }
 
     @Test
@@ -134,6 +170,8 @@ class GroupCreateTest {
         // 3) Call the use case GroupCreate Input Boundary method to run the use case
         // 4) Check that the Output Data passed to the Presenter is correct
         // 5) Check that the expected changes to the data layer are there.
+
+        setUp();
 
         // 1) Instantiate
         GroupCreatePresenter presenter = new GroupCreatePresenter() {
@@ -151,8 +189,8 @@ class GroupCreateTest {
         };
 
 
-        GroupDataInterface groupData = new GroupDataAccess();
-        UserDataInterface userData = new UserDataAccess();
+        GroupDataInterface groupData = new GroupDataAccess("src/test/resources/testgroups.json");
+        UserDataInterface userData = new UserDataAccess("src/test/resources/testusersCopy.json");
         GroupCreateBoundaryIn useCase = new GroupCreate(presenter, groupData, userData);
 
         //setting the data from the database as a constant to check later
@@ -177,7 +215,18 @@ class GroupCreateTest {
         }
 
         // confirm now that the group is in the db
-        assert groupData.groupIdExists("0001");
+        boolean found = false;
+        Map<String, String> groupMap = groupData.getGroupMap();
+        for (Map.Entry<String, String> idEntry : groupMap.entrySet()) {
+            String groupId = idEntry.getKey();
+            Group group = Group.fromString(groupData.groupAsString(groupId));
+            if (group.getGroupName().equals("groupDarcy")){
+                found = true;
+            }
+        }
+        assert found;
+
+        tearDown();
 
     }
 }
