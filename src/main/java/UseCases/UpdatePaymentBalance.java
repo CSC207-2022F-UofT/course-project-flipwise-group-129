7eda53a9b7d461a62cb0a13bf2c6a51b6c752999
rowanArora <rowan.arora@mail.utoronto.ca>
2022-12-04 21:34:33 -0500

    Finished removing all unnecessary code and made sure the code compiles correctly.

--- src/main/java/UseCases/UpdatePaymentBalance.java
+++ src/main/java/UseCases/UpdatePaymentBalance.java
@@ -35,6 +35,7 @@ public class UpdatePaymentBalance implements UpdatePaymentBalanceBoundaryIn{
         String userPurchasedItem = paymentDetails.getUsername();
         float itemPrice = paymentDetails.getItemPrice();
         List<String> usersInvolvedInPurcase = paymentDetails.getUsersInvolvedInPurchase();
+        int amountOfUsersInvolvedInPurchase = usersInvolvedInPurcase.size();
 
         /*
         There are going to be three steps, I can only implement step 2 right now, and then I can
@@ -81,13 +82,6 @@ public class UpdatePaymentBalance implements UpdatePaymentBalanceBoundaryIn{
         catch (RuntimeException e) {
             return raiseError(e);
         }
-//        Set<String> usersInGroup = groupInvolvedInPurchase.getUsers();
-
-        int amountOfUsersInvolvedInPurchase = usersInvolvedInPurcase.size();
-//        List<String> groupUsernames = new ArrayList<>();
-//        for(String user : usersInGroup) {
-//            groupUsernames.add(user);
-//        }
 
         /*
         We can now call getPurchaseBalance() and getAllDebts() on groupInvolvedInPurchase to get the list of all
