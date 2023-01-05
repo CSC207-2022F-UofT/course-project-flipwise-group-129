a4dcb3d589de457794ca6bf159af5a40b8a3fe7d
mishaalkandapath <mishaalhere@gmail.com>
2023-01-05 18:05:52 -0500

    removed html coverage reports

--- JavaDocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Controllers"},{"l":"DataAccess"},{"l":"DataAccessInterface"},{"l":"DataStructures"},{"l":"Entities"},{"l":"InputBoundary"},{"l":"OutputBoundary"},{"l":"Presenters"},{"l":"UseCases"},{"l":"View"}];updateSearchResults();
